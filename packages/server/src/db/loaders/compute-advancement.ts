f93d984bd3f336f32e590636484b2607a4c23dd4
mmathijs <75422017+mmathijs@users.noreply.github.com>
2026-03-13 12:38:21 +0100

    Remove dev logs

--- packages/server/src/db/loaders/compute-advancement.ts
+++ packages/server/src/db/loaders/compute-advancement.ts
@@ -502,9 +502,6 @@ async function computeNormalPlayoffPoints(
     playoffsComplete: boolean;
 }> {
     let playoffPts = new Map<number, { points: number; isFinal: boolean }>();
-    console.log("Computing playoff points for event", eventCode);
-    console.log("Alliances:", alliances);
-    console.log("Playoff matches:", playoffMatches);
     if (!alliances || alliances.length === 0 || playoffMatches.length === 0) {
         return { playoffPts, playoffsComplete: false };
     }
@@ -517,9 +514,6 @@ async function computeNormalPlayoffPoints(
     );
     let playoffsComplete =
         playoffMatches.every((m) => m.hasBeenPlayed) || aliveAlliances.length <= 1;
-
-    console.log("Placement by alliance:", placementByAlliance);
-    console.log("Alive alliances:", aliveAlliances);
 
     if (playoffsComplete && aliveAlliances.length === 1) {
         placementByAlliance.set(aliveAlliances[0]!, { placement: 1, isFinal: true });
