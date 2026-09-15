6c949619142ab6d54b70e9b65b2f43f33885fbb8
MMathijs <mmathijs@users.noreply.github.com>
2026-09-15 12:55:20 +0200

    Add no world record message

--- packages/web/src/routes/+page.svelte
+++ packages/web/src/routes/+page.svelte
@@ -127,8 +127,10 @@
                         event={wrWithPens.event}
                         showHeartLegend={false}
                     />
-                {:else}
+                {:else if !$homeStore}
                     <SkeletonRow header card={false} rows={2} />
+                {:else}
+                    <p class="no-events">No world record yet for this season.</p>
                 {/if}
             </div>
         </div>
