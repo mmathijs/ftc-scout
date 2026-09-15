efe8d8680ee2f1f719fa986b2dfae657e004f9bd
MMathijs <mmathijs@users.noreply.github.com>
2026-09-15 16:13:18 +0200

    Revert enum change, not neccesary anymore

--- packages/common/src/utils/gql/enum.ts
+++ packages/common/src/utils/gql/enum.ts
@@ -3,8 +3,8 @@ import { GraphQLEnumType, GraphQLEnumValueConfigMap } from "graphql";
 export function makeGQLEnum(e: Record<string, string>, name: string): GraphQLEnumType {
     let values: GraphQLEnumValueConfigMap = {};
 
-    for (let [, v] of Object.entries(e)) {
-        values[v] = { value: v };
+    for (let [k, v] of Object.entries(e)) {
+        values[k] = { value: v };
     }
 
     return new GraphQLEnumType({
